492adfc2eb208533298ecb32e0378ce865c8a870
nhoxxonpro <hpvu@uno.edu>
2018-05-15 15:45:44 -0500

    Modified update, show, and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new #instance variable
   end
@@ -16,6 +19,20 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "article was sucessfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
   end
 
   private
