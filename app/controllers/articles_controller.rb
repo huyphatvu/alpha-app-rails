2cd2139a4caad641d3d38ef70aade79443b12df3
Huy Vu <hpvu@uno.edu>
2018-05-18 13:07:40 -0500

    Adding debugging tools and association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    debugger
     @article = Article.new(article_params)
-
+    # @article.user = User.first
     if @article.save
       flash[:notice] = "article was sucessfully created"
       redirect_to article_path(@article)
