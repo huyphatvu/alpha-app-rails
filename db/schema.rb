8d86c260d1a995b818e975d86fd3d16fa72bb5d4
Huy Vu <hpvu@uno.edu>
2018-05-18 11:42:28 -0500

    Create user table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_200324) do
+ActiveRecord::Schema.define(version: 2018_05_18_163617) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,16 +18,6 @@ ActiveRecord::Schema.define(version: 2018_05_16_200324) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "description"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-
 
   create_table "users", force: :cascade do |t|
     t.string "username"
